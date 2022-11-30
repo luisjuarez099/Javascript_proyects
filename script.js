2eb5c9f7e10a83b381548424fce0991019eb83dd
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-11-29 18:58:44 -0600

    Mi prime commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// clase .
+// id #
+//funciones y metodos para seleccionar
+//los elementos que nos permite ejecutar en nuestro html
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('.parrafito');
+const pid = document.getElementById('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value)
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+});

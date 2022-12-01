4ebfdd6c405efa672e600c83a083328e8ef28972
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-12-01 16:54:46 -0600

    Modificando html desde js, agregamos img, manupulamos clases y cambiamos titulo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('#pid');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(input.value)
@@ -16,5 +16,21 @@ console.log({
   pid,
   input,
 });
+// MANIPULACIOND DE JS PARA HTML
+h1.innerHTML="Manipulacion js <br> javascript"//moddificamos html el titulo 
+h1.innerText="" //Interpreta todo como texto. 
 
-h1.innerHTML="Manipulacion js"
+h1.getAttribute="" //leer los atributos de nuestro elementos
+h1.setAttribute="" //modificamos la clase
+
+h1.classList.add("Verde")
+input.value="Carta de Amor"  //Damos por defecto lo que se muestra en la web 
+
+//cremaos elemento desde cero
+const img =document.createElement('img')
+//atributos
+img.setAttribute('src', 'https://www.pngmart.com/files/13/Akatsuki-PNG-Image.png')
+
+console.log(img)
+//insertasmos a un elemento que ya existia
+pid.appendChild(img)

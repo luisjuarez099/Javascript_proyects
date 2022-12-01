18a2f00400f443a151e3d83426375f6d8e251451
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-12-01 17:37:42 -0600

    Utilizamos atributos onclick, onchange, usamos sumas donde el usuario inserta numeros y nosotros lo sumamos mediante funciones en en html escritas en js

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-// clase .
-// id #
-//funciones y metodos para seleccionar
-//los elementos que nos permite ejecutar en nuestro html
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
 
-console.log(input.value)
-console.log({
-  h1,
-  p,
-  parrafito,
-  pid,
-  input,
-});
-// MANIPULACIOND DE JS PARA HTML
-h1.innerHTML="Manipulacion js <br> javascript"//moddificamos html el titulo 
-h1.innerText="" //Interpreta todo como texto. 
+const input1=document.querySelector("#num1")
+const input2=document.querySelector("#num2")
+const btn = document.querySelector("#btnCalcular")
+const presult= document.querySelector("#result")
 
-h1.getAttribute="" //leer los atributos de nuestro elementos
-h1.setAttribute="" //modificamos la clase
+function btnclick(){
 
-h1.classList.add("Verde")
-input.value="Carta de Amor"  //Damos por defecto lo que se muestra en la web 
-
-//cremaos elemento desde cero
-const img =document.createElement('img')
-//atributos
-img.setAttribute('src', 'https://www.pngmart.com/files/13/Akatsuki-PNG-Image.png')
-
-console.log(img)
-//insertasmos a un elemento que ya existia
-pid.appendChild(img)
+  let valores= parseInt(input1.value) + parseInt(input2.value) //los guarda como string
+  presult.innerText= 'Resultado: '+valores;
+  console.log(valores)
+}

0cfa3426eab2b59feafc893d997c3624f9b5487e
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-12-01 17:59:17 -0600

    addactionlistener

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const input1=document.querySelector("#num1")
 const input2=document.querySelector("#num2")
 const btn = document.querySelector("#btnCalcular")
 const presult= document.querySelector("#result")
+//escuchador de eventos que cada vez que sucedan cambios
+//enviar dos eventos(evento a escuchar, funcion - sin parentesis tenidendo los eventos de la funcion)
+//lal funcion no lleva parentesis para que no se ejecute una segunda vez
+btn.addEventListener('click',btnclick);
+
+
 
 function btnclick(){
 

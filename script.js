a8936bff9e84b158b4abd824ba012f1d62152405
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-12-03 11:18:27 -0600

    submit adddactionlistener

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const input1=document.querySelector("#num1")
 const input2=document.querySelector("#num2")
 const btn = document.querySelector("#btnCalcular")
 const presult= document.querySelector("#result")
+const form=document.querySelector("#formulario")
 //escuchador de eventos que cada vez que sucedan cambios
 //enviar dos eventos(evento a escuchar, funcion - sin parentesis tenidendo los eventos de la funcion)
 //lal funcion no lleva parentesis para que no se ejecute una segunda vez
-btn.addEventListener('click',btnclick);
+form.addEventListener('click',sumaclick);
 
 
 
-function btnclick(){
-
+function sumaclick(event){
+  console.log({event})
+  event.preventDefault()//no se recargue la pagina
   let valores= parseInt(input1.value) + parseInt(input2.value) //los guarda como string
   presult.innerText= 'Resultado: '+valores;
   console.log(valores)
